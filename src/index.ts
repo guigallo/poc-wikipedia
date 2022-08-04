f7f8550b83a141ccd07ff566dc905279c5ec3fcb
guigallo <guilherme.gg1@hotmail.com>
2022-08-04 16:19:47 -0300

    feat: search and show table

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-function main() {
-  console.log('hello world');
+import wikipedia from 'wikipedia'
+
+async function main() {
+  wikipedia.setLang('pt');
+
+  const search = await wikipedia.search('Lista de monarcas da Inglaterra', {})
+  console.log(search);
+
+  const result = search.results.find((r) => r.title === 'Lista de monarcas da Inglaterra')
+  const page = await wikipedia.page(result.pageid);
+  const summary = await page.summary();
+  const tables = await page.tables();
+  console.log(summary, tables);
+
+  // const batman = await wikipedia.page('Lista_de_monarcas_da_Inglaterra');
+  // const summary = await batman.summary();
+  // console.log(batman, summary);
+  // const tables = await wikipedia.tables('Lista_de_monarcas_da_Inglaterra', {});
+  // console.log(tables);
 }
 
-main();
+main()
